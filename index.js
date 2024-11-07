7f19b8ff2a23ffdd079f368ba9c01c09e95a9b2a
ingridevv <personnel.ingrid@gmail.com>
2024-11-07 08:38:10 -0300

    Inclusão do Input e Output, Finalizaçaão

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 class Hero {
   static ataques = {
     mago: "Magia",
@@ -7,30 +14,46 @@ class Hero {
     bruxo: "Expelliarmus",
   };
 
-  constructor(nome, idade, tipo) {
+  constructor(nome, nivel, tipo) {
     this.nome = nome;
-    this.idade = idade;
+    this.nivel = nivel;
+    this.tipo = tipo.toLowerCase(); // Garantir que o tipo esteja em minúsculas
 
     // Para verificar se o tipo é um texto
-    if (typeof tipo != "string") {
+    if (typeof tipo !== "string") {
       throw new Error("O tipo deve ser um texto");
     }
 
-    // Conversão de maíusculas para minúsculas
-    this.tipo = tipo;
+    // Verificar se o tipo existe no objeto de ataques
+    if (!Hero.ataques[this.tipo]) {
+      throw new Error(`Tipo de herói inválido: ${this.tipo}`);
+    }
   }
 
   atacar() {
-    let ataque = Hero.ataques[this.tipo.toLowerCase()];
-
-    if (!ataque) {
-      ataque = "Ataque falhou";
-    }
-
-    // mensagem do ataque
-    console.log(`o ${this.tipo} atacou usando ${ataque}`);
+    const ataque = Hero.ataques[this.tipo];
+    console.log(`O ${this.tipo} atacou usando ${ataque}`);
   }
 }
 
-let hero = new Hero("yoda", 100, "guerreiro");
-hero.atacar(); // mago ataca usando magia
+rl.question("Digite o nome do herói: ", (nomeInput) => {
+  rl.question("Digite o nível do herói: ", (nivelInput) => {
+    rl.question(
+      "Digite o tipo de herói (Mago, Guerreiro, Monge, Ninja, Bruxo): ",
+      (tipoInput) => {
+        const nome = nomeInput;
+        const nivel = parseInt(nivelInput); // Convertenivel para número
+        const tipo = tipoInput.trim().toLowerCase(); // Converte tipo para minúsculas
+
+        try {
+          const heroi = new Hero(nome, nivel, tipo);
+          heroi.atacar();
+        } catch (error) {
+          console.error(error.message);
+        }
+
+        rl.close();
+      }
+    );
+  });
+});
